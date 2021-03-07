e86ddf5a7f13bf76ed75f64373b10f19d8de0a00
Will <doggy.huang@gmail.com>
2021-03-07 11:25:36 +0800

    加入 Footer 元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
+    FooterComponent,
     AppComponent,
     HeaderComponent
   ],
